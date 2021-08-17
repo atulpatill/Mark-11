43da8c98c4b363e6227fac6ec83a280c204c4900
Atul Patil <atulpatil539@gmail.com>
2021-08-17 16:36:55 +0530

    feat: added

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberOfButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
+
+function compareValues(sum,luckyNumber){
+  if (sum%luckyNumber===0){
+    outputBox.innerText ="Hurray!!!Your Birthday is Lucky😀";
+  }else{
+    outputBox.innerText="Sorry!!! Your Birthday is not Lucky😐";
+  }
+}
 
 
  function checkBirthDateIsLucky(){
-  const date = dateOfBirth.value ;
-    const sum = calculateSum(date);
-  console.log(sum)
+  const dob = dateOfBirth.value ;
+    const sum = calculateSum(dob);
+    if (sum&&dob){
+      compareValues(sum, luckyNumber.value)
+    } else{
+      outputBox.innerText = "Ohh!!! Please enter both the fields😮"
+    }
+  
+ 
 }
 
-function calculateSum(date){
-   return 12;
+function calculateSum(dob){
+  dob = dob.replaceAll("-","");
+  let sum = 0;
+  for(let i=0; i<dob.length; i++){
+  sum = sum + Number(dob.charAt(i));
+  }
+  return sum;
 }
 
 checkNumberOfButton.addEventListener("click", checkBirthDateIsLucky)
